be6882f3d3a9afb35d78b7870b4e7e8fcf62e6d0
ladni <ladnik@hotmail.com>
2020-03-23 11:32:19 +0200

    UUpdate after learning progeress - 1

--- gql02/src/app.ts
+++ gql02/src/app.ts
@@ -1,9 +1,8 @@
-// const express = require('express');
-// const graphqlHTTP = require('express-graphql');
-// import {buildSchema, GraphQLSchema} from 'graphql';
-// import { RequestInfo } from "express-graphql";
-
-import { Person } from 'schema-dts';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema, GraphQLSchema } from 'graphql';
+import { RequestInfo } from "express-graphql";
+import { Person, School, Organization } from 'schema-dts';
 
 const p: Person = {
     '@type': 'Person',
@@ -15,5 +14,45 @@ const p: Person = {
 
 (function main() {
     let str = p.name;
+
+    let schema = buildSchema(`
+        type Query {
+            getPersons(schoolName: String!): [Person]
+        }
+    `);
+
+    // let resolver = {
+    //     //getDie: ({numSides}) => {
+    //     getDie: (parent: any, args: any, context: any, info: GraphQLResolveInfo) =>  
+    //       new RandomDie1(parent.numSides)
+    // }
+
+      let resolver = {
+        getPersons: (args: any, request: any, schemaQ: any/*GraphQLSchema*/) => {
+            // let yy: any;
+            // if (schemaQ) {
+            //     for (let i = 0; i < schemaQ.fieldNodes.length; i++) {
+            //         let node = schemaQ.fieldNodes[i];
+            //         let val = node.name.value;
+            //     }
+            // }
+            // let query = request.query;
+            // let url = request.url;
+            // let baseUrl = request.baseUrl;
+            // let originalUrl = request.originalUrl;
+            // }
+            return [p, p];
+        },
+    };  
+
+    let app = express();
+    app.use('/graphql', graphqlHTTP({
+        schema,
+        rootValue: resolver,
+        graphiql: true,
+    }));
+
+    app.listen(4000);
+    console.log('Running a GraphQL API server at localhost:4000/graphql');
 })();
 
---
+++ tut01/schema/schema.js
@@ -0,0 +1,53 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLSchema,
+	GraphQLID
+} = graphql;
+
+// dummy data
+let books = [
+	{ name: "Aaa", genre: 'Fantasy', id: '1' },
+	{ name: "Bbb", genre: 'Fantasy', id: '2' },
+	{ name: "Ccc", genre: 'Sci-Fi', id: '3' },
+];
+
+const BookType = new GraphQLObjectType({
+	name: 'Book',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		genre: { type: GraphQLString },
+	})
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		book: {
+			type: BookType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				// code to get data from db / other source - this is resolver
+				return _.find(books, { id: args.id });
+			}
+		}
+	}
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
+});
+
+/*
+query {
+  book(id: 2) {
+    name
+    genre
+    id
+  }
+}
+ */
